2c2a95f4be052ad11064df480f155afef77802d3
wduntak <wduntak@gmail.com>
2020-02-18 22:23:11 -0500

    Update gatsby-node configs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-// graphql function doesn't throw an error so we have to check to check for the result.errors to throw manually
 const wrapper = promise =>
     promise.then(result => {
         if (result.errors) {
@@ -16,20 +15,6 @@ exports.createPages = async ({ graphql, actions }) => {
         graphql(`
         {
             prismic {
-                // allProjects {
-                //     edges {
-                //         node {
-                //             project_title
-                //             project_preview_description
-                //             project_preview_thumbnail
-                //             project_category
-                //             project_post_date
-                //             _meta {
-                //                 uid
-                //             }
-                //         }
-                //     }
-                // }
                 allPosts {
                     edges {
                         node {
@@ -52,25 +37,9 @@ exports.createPages = async ({ graphql, actions }) => {
     `)
     )
 
-    // const projectsList = result.data.prismic.allProjects.edges;
     const postsList = result.data.prismic.allPosts.edges;
 
-    // const projectTemplate = require.resolve('./src/templates/project.jsx');
     const postTemplate = require.resolve('./src/templates/post.jsx');
-
-    // projectsList.forEach(edge => {
-    //     // The uid you assigned in Prismic is the slug!
-    //     createPage({
-    //         type: 'Project',
-    //         match: '/work/:uid',
-    //         path: `/work/${edge.node._meta.uid}`,
-    //         component: projectTemplate,
-    //         context: {
-    //             // Pass the unique ID (uid) through context so the template can filter by it
-    //             uid: edge.node._meta.uid,
-    //         },
-    //     })
-    // })
 
     postsList.forEach(edge => {
         createPage({
